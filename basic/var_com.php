9a62d67698bb57eb1910344bd95458fc4a615663
Lourena Ohara <lourena6ohara@gmail.com>
2023-09-11 20:41:45 -0300

    Add files via upload

---
+++ basic/var_com.php
@@ -0,0 +1,16 @@
+<?php
+//variáveis: int e float
+$x = 5;
+$y = 10.5;
+
+/*
+    Variáveis e impresão
+    de variáveis
+*/
+
+$txt = "W3Schools.com";
+echo "I love $txt!";
+
+$a = 5;
+$b = 4;
+echo $a + $b;
